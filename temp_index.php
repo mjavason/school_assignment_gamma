50b2b4833d32acd0b5a08fa22c17aba2b26ab4ce
Orji Micheal <orjimichael4886@gmail.com>
2022-10-14 14:08:44 +0100

    Created a temporary almost empty page as reference for creating new pages

---
+++ temp_index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+
+<head>
+
+	<!-- Basic -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Demo SEO | Porto - Multipurpose Website Template</title>
+
+	<meta name="keywords" content="WebSite Template" />
+	<meta name="description" content="Porto - Multipurpose Website Template">
+	<meta name="author" content="okler.net">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+	<!-- Web Fonts  -->
+	<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&amp;display=swap" rel="stylesheet" type="text/css">
+
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+	<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+	<link rel="stylesheet" href="css/theme-blog.css">
+	<link rel="stylesheet" href="css/theme-shop.css">
+
+	<!-- Demo CSS -->
+	<link rel="stylesheet" href="css/demos/demo-seo.css">
+
+	<!-- Skin CSS -->
+	<link id="skinCSS" rel="stylesheet" href="css/skins/skin-seo.css">
+
+	<!-- Theme Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+
+	<!-- Head Libs -->
+	<script src="vendor/modernizr/modernizr.min.js"></script>
+
+
+
+</head>
+
+<body data-plugin-scroll-spy data-plugin-options="{'target': '#header'}">
+
+	<div class="body">
+
+		<!-- header -->
+		<?php require_once('includes/header.php') ?>
+
+		<div role="main" class="main">
+
+			<div class="container position-relative py-5" style="min-height: 643px;" id="home">
+				<svg class="custom-svg-style-1" viewBox="0 0 180 470" x="0px" y="0px" width="100%" height="100%">
+					<g>
+						<path id="shape_one_start" opacity="0.098" fill="#00a3d5" d="M 847.5703 23.7939 C 847.5703 23.7939 581.8154 -245.1675 394.2842 33.9375 C 305.4043 166.2183 345.0537 358.8125 253.5615 364.6631 C 162.0684 370.5146 129.5303 342.542 70.9883 383.9072 C 3.0254 431.9297 17.8926 499.2158 17.8926 499.2158 L 847.5703 499.2158 L 847.5703 23.7939 Z" />
+						<path id="shape_one_end" opacity="0.098" fill="#00a3d5" d="m847.5703,24.7939c0,0 -85.7549,-346.9614 -273.2861,-67.8564c149.1201,176.2808 52.7695,313.875 -46.7227,312.7256c-99.4922,-1.1494 -59.0312,107.8789 -117.5732,149.2441c-67.9629,48.0225 -186.0957,167.3086 -207.0957,167.3086l644.6777,-86l0,-475.4219z" style="visibility:hidden" />
+
+						<path id="shape_two_start" opacity="0.149" fill="#00a3d5" d="M 847.5703 239.9771 C 847.5703 239.9771 806.873 141.5913 733.1348 164.937 C 622.249 200.0459 648.416 318.083 566.6426 342.042 C 484.8682 365.999 247.9814 345.0283 168.6406 371.8037 C 89.3008 398.5781 85.5703 499.3408 85.5703 499.3408 L 847.5693 499.3408 L 847.5703 239.9771 Z" />
+						<path id="shape_two_end" opacity="0.149" fill="#00a3d5" d="m907.5703,89.9771c0,0 29.3027,-9.3858 -95.4355,112.9599c-124.7382,122.3457 -254.7188,64.146 -300.4922,167.105c-45.7734,102.959 -79.6612,61.9863 -209.002,106.7617c-129.3408,44.7754 -218.0703,21.5371 -218.0703,21.5371l761.999,0l0.001,-265.3637z" style="visibility:hidden" />
+					</g>
+				</svg>
+				<div class="row align-items-center py-5 p-relative z-index-1">
+					<div class="col-lg-6 pt-4 pt-lg-0 mt-5 mt-lg-0 mb-5 mb-lg-0">
+						<div class="appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">
+							<h1 class="mb-4 mb-md-0 mb-xl-3" data-plugin-float-element data-plugin-options="{'startPos': 'none', 'speed': 1, 'transition': true, 'horizontal': false}">
+								<strong>Start growing your business</strong> through better SEO today
+							</h1>
+						</div>
+						<div class="appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+							<p class="text-4 mb-5 mb-md-2" data-plugin-float-element data-plugin-options="{'startPos': 'none', 'speed': 0.5, 'transition': true, 'horizontal': false}">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+								has been the industry's standard dumm.</p>
+						</div>
+						<div class="appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="800">
+							<a data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#pricing" class="btn btn-primary btn-outline btn-rounded font-weight-semibold text-4 btn-px-5 btn-py-2" data-plugin-float-element data-plugin-options="{'startPos': 'none', 'speed': 0.3, 'transition': true, 'horizontal': false}">Get
+								Started Now</a>
+						</div>
+					</div>
+					<div class="col-lg-6 text-center mt-5 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="550">
+						<img src="img/demos/seo/seo-charts.png" class="img-fluid" alt="" data-plugin-float-element data-plugin-options="{'startPos': 'none', 'speed': 8, 'transition': true, 'horizontal': true}" />
+					</div>
+				</div>
+			</div>
+
+			
+
+			<!-- <section class="section border-0 m-0">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="owl-carousel owl-theme carousel-center-active-item mb-0"
+								data-plugin-options="{'responsive': {'0': {'items': 1}, '476': {'items': 1}, '768': {'items': 5}, '992': {'items': 5}, '1200': {'items': 5}}, 'autoplay': true, 'autoplayTimeout': 3000, 'stagePadding': 0, 'dots': false, 'nav': false}">
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-1.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-2.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-3.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-4.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-5.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-6.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-4.png" alt="">
+								</div>
+								<div class="px-3">
+									<img class="img-fluid" src="img/logos/logo-2.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section> -->
+
+			
+		</div>
+
+		<!-- footer		 -->
+		<?php require_once('includes/footer.php') ?>
+
+
+	</div>
+
+
+	<!-- Vendor -->
+	<script src="vendor/plugins/js/plugins.min.js"></script>
+	<script src="vendor/kute/kute.min.js"></script>
+
+	<!-- Theme Base, Components and Settings -->
+	<script src="js/theme.js"></script>
+
+	<!-- Current Page Vendor and Views -->
+	<script src="js/views/view.contact.js"></script>
+
+	<!-- Demo -->
+	<script src="js/demos/demo-seo.js"></script>
+
+	<!-- Theme Initialization Files -->
+	<script src="js/theme.init.js"></script>
+
+</body>
+
+</html>
