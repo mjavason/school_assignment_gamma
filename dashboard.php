cdda4289f58a9372121cd254b1d72e6843d7c9a1
Orji Micheal <orjimichael4886@gmail.com>
2022-10-21 09:30:21 +0100

    Added dashboard links for years

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@ require_once('config/connect.php');
 require_once('functions/functions.php');
 
 if (!isset($_SESSION['log'])) {
-    gotoPage("sign-in.php");
+	gotoPage("sign-in.php");
 }
 ?>
 <!DOCTYPE html>
@@ -35,11 +35,63 @@ if (!isset($_SESSION['log'])) {
 			<div class="container position-relative py-5" style="min-height: 643px;" id="home">
 				<?php require_once('includes/svg_animation.php') ?>
 				<div class="row align-items-center py-5 p-relative z-index-1">
-					<h1>User Dashboard</h1>
+					<h1 class="card-title mb-1 font-weight-bold transition-2ms">Years</h1>
+
+
+					<div class="col-md-6 p-1 col-lg-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+						<div class="card bg-color-grey card-text-color-hover-light border-0 bg-color-hover-primary transition-2ms box-shadow-1 box-shadow-1-primary box-shadow-1-hover">
+							<div class="card-body">
+								<h4 class="card-title mb-1 text-4 font-weight-bold transition-2ms">Year 1</h4>
+								<!-- <p class="card-text transition-2ms">Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit. Curabitur rhoncus nulla dui, in dapi.</p> -->
+									
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 p-1 col-lg-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+						<div class="card bg-color-grey card-text-color-hover-light border-0 bg-color-hover-primary transition-2ms box-shadow-1 box-shadow-1-primary box-shadow-1-hover">
+							<div class="card-body">
+								<h4 class="card-title mb-1 text-4 font-weight-bold transition-2ms">Year 2</h4>
+								<!-- <p class="card-text transition-2ms">Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit. Curabitur rhoncus nulla dui, in dapi.</p> -->
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 p-1 col-lg-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+						<div class="card bg-color-grey card-text-color-hover-light border-0 bg-color-hover-primary transition-2ms box-shadow-1 box-shadow-1-primary box-shadow-1-hover">
+							<div class="card-body">
+								<h4 class="card-title mb-1 text-4 font-weight-bold transition-2ms">Year 3</h4>
+								<!-- <p class="card-text transition-2ms">Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit. Curabitur rhoncus nulla dui, in dapi.</p> -->
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 p-1 col-lg-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+						<div class="card bg-color-grey card-text-color-hover-light border-0 bg-color-hover-primary transition-2ms box-shadow-1 box-shadow-1-primary box-shadow-1-hover">
+							<div class="card-body">
+								<h4 class="card-title mb-1 text-4 font-weight-bold transition-2ms">Year 4</h4>
+								<!-- <p class="card-text transition-2ms">Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit. Curabitur rhoncus nulla dui, in dapi.</p> -->
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 p-1 col-lg-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+						<div class="card bg-color-grey card-text-color-hover-light border-0 bg-color-hover-primary transition-2ms box-shadow-1 box-shadow-1-primary box-shadow-1-hover">
+							<div class="card-body">
+								<h4 class="card-title mb-1 text-4 font-weight-bold transition-2ms">Year 5</h4>
+								<!-- <p class="card-text transition-2ms">Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit. Curabitur rhoncus nulla dui, in dapi.</p> -->
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
-			
+
 
 
 		</div>
